5c01efb75ba8285e9906777fdcb886c5bc057bfe
Paolo Usero <julianpaolousero@gmail.com>
2016-10-04 03:28:11 -0400

    more comprehensive tool
    Accounts for scripts ran at creation time

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 # - - - - - - - - - - - - - - - - - - - - 
-# TODO: MAKE REFERENCE NODE FROM CURRENT SELECTION
+# ParmCompTool
+# Compares parameters to both default parameters and initial script parameters (not necessarily default)
+# TODO: 
 # - - - - - - - - - - - - - - - - - - - - 
 import traceback
 nodes = hou.selectedNodes()
@@ -7,6 +9,8 @@ nodes = hou.selectedNodes()
 def formatprint(name, current, default, folders,templateType):
 	print '{:20}:\t{:10}\t{:10}\t{:20}\t{:20}'.format(name, current, default,folders,templateType)
 
+
+print 'after'
 # iterate through selected nodes
 print '\nNon Default Parameters-----------------------------------------'
 for  node in nodes:
@@ -14,21 +18,23 @@ for  node in nodes:
 	print node.path()
 	formatprint(str('name'), str('currentVal'), str('defaultVal'), str('Folder'), str('type or parent param'))	
 	parms = node.parmTuples()
+	# make comparison node
+	cache = node.parent().createNode( node.type().name())
 
 	# iterate through parameters of current node
-	for parm in parms:
-		if not parm.isAtDefault():
+	for parm,defaultParm in map(None,parms,cache.parmTuples()):
+		if not parm.isAtDefault() and  not defaultParm.eval() == parm.eval():
 			# organize and gather data
 			parmPath = parm.node().path()
 			name =  parm.parmTemplate().label()
 			currentValue = parm.eval()
-
+			parmType = parm.parmTemplate().type()
 			try:
 				defaultValue = parm.parmTemplate().defaultValue()
+
 			except AttributeError:
 				defaultValue = None
 
-			parmType = parm.parmTemplate().type()
 			#print data
 			if str(parmType) == 'parmTemplateType.Int' or str(parmType) == 'parmTemplateType.Float': 	
 				for current, default in zip(currentValue,defaultValue):
@@ -56,9 +62,11 @@ for  node in nodes:
 				current = currentValue[0]
 				default = defaultValue[0]
 				print formatprint(name, current, default,parm[0].containingFolders(),parm[0].parmTemplate().type())
-				"""
-			if (str(parmType) == 'parmTemplateType.FolderSet' or
+				if (str(parmType) == 'parmTemplateType.FolderSet' or
 					str(parmType) == 'parmTemplateType.Button' or
 				  	str(parmType) == 'parmTemplateType.FolderSet' or
 				   	str(parmType) == 'parmTemplateType.Separator' or
 				    str(parmType) == 'parmTemplateType.Label'):
+
+				"""
+	cache.destroy()
