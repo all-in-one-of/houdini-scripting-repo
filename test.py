d450cc2ffaaa29800da2e014da6755e47aa550c6
Paolo Usero <julianpaolousero@gmail.com>
2016-10-11 22:05:38 -0400

    Compare to other Node, formatting

--- test.py
+++ test.py
@@ -6,79 +6,87 @@
 import traceback
 nodes = hou.selectedNodes()
 
-def formatprint(name, current, default, folders,templateType):
-	print '{:20}:\t{:10}\t{:10}\t{:20}\t{:20}'.format(name, current, default,folders,templateType)
+def formatprint(name, current, default, templateType, folders ):
+        print '{:20.20}\t{:<15}\t{:<15}\t{:10.10}\t{:20}'.format( name, current, default, str(templateType).split('parmTemplateType.').pop(), folders )
 
 #Check both parm template default and script initialization defaults
 def isNonDefault(parm, defaultParm):
-	if parm is not None and defaultParm is not None: 
-		if not parm.eval() == defaultParm.eval():
-			return True
-		else:
-			return False
-	# spare parameters, essentially
-	elif parm is not None:
-		return True
-	else:
-		return False
+        if parm is not None and defaultParm is not None: 
+                if not parm.eval() == defaultParm.eval():
+                        return True
+                else:
+                        return False
+        # spare parameters, essentially
+        elif parm is not None:
+                return True
+        else:
+                return False
 
-print 'after'
 # iterate through selected nodes
-print '\nNon Default Parameters-----------------------------------------'
+print '\nNon Default Parameters' + '-' * 80
 for  node in nodes:
-	print '\n'
-	print node.path()
-	formatprint(str('name'), str('currentVal'), str('defaultVal'), str('Folder'), str('type or parent param'))	
-	parms = node.parmTuples()
-	# make comparison node
-	cache = node.parent().createNode( node.type().name())
+        print '\n'
+        print (node.path())
+        print '- ' * 40
+        formatprint(str('NAME'), str('CURRENT VAL'), str('DEFAULT VAL'), str('TYPE / PARENT'), str('FOLDER'))
+        parms = node.parmTuples()
+        # make comparison node
+        cache = node.parent().createNode( node.type().name())
+        oldfolders = None
+        
+        # iterate through parameters of current node
+        for parm,defaultParm in map(None,parms,cache.parmTuples()):
+                try: 
+                        folders = parm[0].containingFolders()[0]
+                except IndexError:
+                        folders = None
+                if isNonDefault(parm,defaultParm):
+                        # organize and gather data
+                        parmPath = parm.node().path()
+                        name =  parm.parmTemplate().label()
+                        currentValue = parm.eval()
+                        parmType = parm.parmTemplate().type()
+                        try:
+                                defaultValue = parm.parmTemplate().defaultValue()
 
-	# iterate through parameters of current node
-	for parm,defaultParm in map(None,parms,cache.parmTuples()):
-		if isNonDefault(parm,defaultParm):
-			# organize and gather data
-			parmPath = parm.node().path()
-			name =  parm.parmTemplate().label()
-			currentValue = parm.eval()
-			parmType = parm.parmTemplate().type()
-			try:
-				defaultValue = parm.parmTemplate().defaultValue()
+                        except AttributeError:
+                                defaultValue = None
 
-			except AttributeError:
-				defaultValue = None
+                        #print data
+                        if str(parmType) == 'parmTemplateType.Ramp':
+                        #       formatprint(name, current, default,parm[0].containingFolders(),parm[0].parmTemplate().type())
+                                """
+                                current = currentValue[0]
+                                default = defaultValue[0]
+                                print formatprint(name, current, default,parm[0].containingFolders(),parm[0].parmTemplate().type())
+                                if (str(parmType) == 'parmTemplateType.FolderSet' or
+                                        str(parmType) == 'parmTemplateType.Button' or
+                                        str(parmType) == 'parmTemplateType.Ramp' or
+                                        str(parmType) == 'parmTemplateType.FolderSet' or
+                                        str(parmType) == 'parmTemplateType.Separator' or
+                                    str(parmType) == 'parmTemplateType.Label'):
 
-			#print data
-			if str(parmType) == 'parmTemplateType.Int' or str(parmType) == 'parmTemplateType.Float': 	
-				for current, default in zip(currentValue,defaultValue):
-					if(parm.isMultiParmInstance()):
-						formatprint(name, current, default, parm[0].containingFolders(), parm.parentMultiParm().name())
-					else:
-						formatprint(name, current, default,parm[0].containingFolders(),parm[0].parmTemplate().type())
-			if str(parmType) == 'parmTemplateType.Menu':
-				labels = parm.parmTemplate().menuLabels()
-				current = labels[currentValue[0]]
-				default = labels[defaultValue]
-				formatprint(name, current, default,parm[0].containingFolders(),parm[0].parmTemplate().type())
-			if str(parmType) == 'parmTemplateType.Toggle':
-				current = str(bool(currentValue[0]))
-				default = str(defaultValue)
-				formatprint(name, current, default,parm[0].containingFolders(),parm[0].parmTemplate().type())
-			if str(parmType) == 'parmTemplateType.String':
-				current = currentValue[0]
-				default = defaultValue[0]
-				formatprint(name, current, default,parm[0].containingFolders(),parm[0].parmTemplate().type())
-			if str(parmType) == 'parmTemplateType.Ramp':
-			#	formatprint(name, current, default,parm[0].containingFolders(),parm[0].parmTemplate().type())
-				"""
-				current = currentValue[0]
-				default = defaultValue[0]
-				print formatprint(name, current, default,parm[0].containingFolders(),parm[0].parmTemplate().type())
-				if (str(parmType) == 'parmTemplateType.FolderSet' or
-					str(parmType) == 'parmTemplateType.Button' or
-					str(parmType) == 'parmTemplateType.Ramp' or
-				  	str(parmType) == 'parmTemplateType.FolderSet' or
-				   	str(parmType) == 'parmTemplateType.Separator' or
-				    str(parmType) == 'parmTemplateType.Label'):
-
-				"""
-	cache.destroy()
+                                """
+                        elif not oldfolders == folders:
+                                print '- ' * 20
+                        if str(parmType) == 'parmTemplateType.Int' or str(parmType) == 'parmTemplateType.Float':        
+                                for current, default in zip(currentValue,defaultValue):
+                                        if(parm.isMultiParmInstance()):
+                                                formatprint( name, current, default, parm.parentMultiParm().name(), parm[0].containingFolders())
+                                        else:
+                                                formatprint(name, current, default, parm[0].parmTemplate().type(), parm[0].containingFolders() )
+                        if str(parmType) == 'parmTemplateType.Menu':
+                                labels = parm.parmTemplate().menuLabels()
+                                current = labels[currentValue[0]]
+                                default = labels[defaultValue]
+                                formatprint(name, current, default, parm[0].parmTemplate().type(), parm[0].containingFolders() )
+                        if str(parmType) == 'parmTemplateType.Toggle':
+                                current = str(bool(currentValue[0]))
+                                default = str(defaultValue)
+                                formatprint(name, current, default, parm[0].parmTemplate().type(), parm[0].containingFolders() )
+                        if str(parmType) == 'parmTemplateType.String':
+                                current = currentValue[0]
+                                default = defaultValue[0]
+                                formatprint(name, current, default, parm[0].parmTemplate().type(), parm[0].containingFolders() )
+                        oldfolders = folders
+        cache.destroy()
